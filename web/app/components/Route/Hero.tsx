2c0f0eac36fb8f37fbd978b0181b0855556d96ab
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-07-03 21:07:31 +0200

    add hero search bar name

--- web/app/components/Route/Hero.tsx
+++ web/app/components/Route/Hero.tsx
@@ -13,7 +13,8 @@ const Hero: FC<Props> = (props) => {
         <Image
           src={require("../../../public/assets/banner-img-1.png")}
           alt=""
-          className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"        />
+          className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"
+        />
       </div>
       <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
         <h2 className="dark:text-white resize-none text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[60px] 1500px:text-[70px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[60%] 1100px:w-[78%] outline-none bg-transparent block">
@@ -28,6 +29,7 @@ const Hero: FC<Props> = (props) => {
         <br />
         <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] h-[50px] bg-transparent relative">
           <input
+            name="hero-search"
             type="search"
             placeholder="Search Courses..."
             className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-[#0000004e] dark:text-[#ffffffe6] text-[20px] font-[500]"
